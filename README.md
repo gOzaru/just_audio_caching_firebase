303818a635c1ada996ef7794705eab5b46428cce
Chowchow <gOzaru@users.noreply.github.com>
2023-03-16 20:45:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# just_audio_caching_firebase
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/just_audio_caching_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.just_audio_caching_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
